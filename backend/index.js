969a779229a876a3890b5d9a5e323c2b65fd2ce3
AniruddhaGawali <aniruddhagawali05@gmail.com>
2022-12-21 20:14:00 +0530

    added skills api

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 8888;
 
 const projects = require("./api/projects");
 const intro = require("./api/intro");
+const skill = require("./api/skills");
 
 app.get("/", (req, res) => {
   res.send("Working");
@@ -11,5 +12,6 @@ app.get("/", (req, res) => {
 
 app.use("/api/projects", projects);
 app.use("/api/intro", intro);
+app.use("/api/skill", skill);
 
 app.listen(port);
