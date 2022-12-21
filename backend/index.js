f1b49fed96a22761ec682943b96089870e0d16c6
AniruddhaGawali <73152642+AniruddhaGawali@users.noreply.github.com>
2022-12-21 18:29:09 +0530

    Development (#9)
    * added slider
    
    * added flip card
    
    * update the flip card
    
    * added backend
    
    * added new color in global
    
    * remove the unused module
    
    * add some ui changes
    
    * update the swiper js according to width
    
    * update gitignore
    
    * remove backend
    
    * update the var
    
    * remove hook
    
    * temp
    
    * temp
    
    * temp
    
    * added new backend
    
    * added vercel file
    
    * update vercel.json
    
    * update the backend
    
    * added test
    
    * update git ignore
    
    * remove backend test
    
    * update api link
    
    * update card viewport
    
    * update  import of projects
    
    * fix  the error
    
    * added smooth scrolling
    
    * fix the ui for mobile
    
    * set color for navigation
    
    * added flip icon
    
    * added intro
    
    * try to fix the backend

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8888;
 
 const projects = require("./api/projects");
 const intro = require("./api/intro");
 
 app.get("/", (req, res) => {
   res.send("Working");
+});
 
 app.use("/api/projects", projects);
 app.use("/api/intro", intro);
